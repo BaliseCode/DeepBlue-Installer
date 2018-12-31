4baf483c6a1e33b80cdbe37af8cdaf5b870b6eff
Unknown <simon@imagerie.ca>
2018-12-31 12:58:32 -0500

    Took stuff from composer Intallers

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,7 +7,7 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
 class Plugin implements PluginInterface {
-
+ 
     public function activate(Composer $composer, IOInterface $io) {
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
